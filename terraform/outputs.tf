2d2eded699f4cad37d59cd7a6460421e5f93f280
ThanhHCSigmaXYZ <thoang@sigmaxyz.com>
2026-03-05 04:07:03 +0900

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,12 +3,17 @@ output "dataset_id" {
   description = "Created BigQuery dataset"
 }
 
-output "repository_name" {
+output "dataform_repository" {
   value       = google_dataform_repository.repo.name
   description = "Created Dataform repository"
 }
 
-output "service_account_email" {
+output "service_account" {
   value       = google_service_account.dataform_sa.email
   description = "Service Account email"
+}
+
+output "dataform_console_url" {
+  value       = "https://console.cloud.google.com/bigquery/dataform/locations/${var.region}/repositories/${google_dataform_repository.repo.name}?project=${var.project_id}"
+  description = "Dataform Console URL"
 }
